2e082c0199e4ab199e0edf90ff5c39b3bb69d9ca
BooksCup <812809680@qq.com>
2020-08-12 10:34:54 +0800

    新增同步用户至极光的逻辑

--- src/main/java/com/bc/wechat/server/controller/admin/AdminJimUserController.java
+++ src/main/java/com/bc/wechat/server/controller/admin/AdminJimUserController.java
@@ -3,7 +3,9 @@ package com.bc.wechat.server.controller.admin;
 import cn.jiguang.common.resp.APIConnectionException;
 import cn.jiguang.common.resp.APIRequestException;
 import cn.jmessage.api.JMessageClient;
+import com.bc.wechat.server.entity.User;
 import com.bc.wechat.server.enums.ResponseMsg;
+import com.bc.wechat.server.service.UserService;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 
 /**
@@ -27,6 +31,9 @@ public class AdminJimUserController {
 
     @Autowired
     private JMessageClient jMessageClient;
+
+    @Resource
+    private UserService userService;
 
     /**
      * 检测用户是否在极光中存在
@@ -55,6 +62,32 @@ public class AdminJimUserController {
             e.printStackTrace();
             responseEntity = new ResponseEntity<>(
                     ResponseMsg.USER_NOT_EXISTS.getResponseCode(), HttpStatus.BAD_REQUEST);
+        }
+        return responseEntity;
+    }
+
+    /**
+     * 同步用户至极光
+     *
+     * @param userId 用户ID
+     * @return ResponseEntity<String>
+     */
+    @ApiOperation(value = "同步用户至极光", notes = "同步用户至极光")
+    @PostMapping(value = "")
+    public ResponseEntity<String> syncUserToJim(@RequestParam String userId) {
+        logger.info("[syncUserToJim], userId: " + userId);
+        ResponseEntity<String> responseEntity;
+        try {
+            User user = userService.getUserByUserId(userId);
+            jMessageClient.registerAdmins(user.getUserId(), user.getUserImPassword());
+            jMessageClient.updateUserInfo(user.getUserId(), user.getUserNickName(),
+                    "1970-01-01", user.getUserSign(),
+                    0, "", "", user.getUserAvatar());
+            responseEntity = new ResponseEntity<>(
+                    ResponseMsg.SYNC_USER_TO_JIM_SUCCESS.getResponseCode(), HttpStatus.OK);
+        } catch (Exception e) {
+            responseEntity = new ResponseEntity<>(
+                    ResponseMsg.SYNC_USER_TO_JIM_ERROR.getResponseCode(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return responseEntity;
     }
